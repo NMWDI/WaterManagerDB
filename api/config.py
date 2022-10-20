8ba59e1452a002acf71f70a772d82ac783b26baf
Chris Cox <chrisc@dyacon.com>
2022-10-20 12:04:45 -0600

    Minor change to sqlalchemy conn string and example .env file

--- api/config.py
+++ api/config.py
@@ -34,7 +34,7 @@ class Settings:
         "POSTGRES_PORT", 5432
     )  # default postgres port is 5432
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
 settings = Settings()
