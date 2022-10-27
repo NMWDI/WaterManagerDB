75edbc23466e3512c01567f8956b4377dd004d0f
Chris Cox <chrisc@dyacon.com>
2022-10-27 11:06:17 -0600

    Minor changes to home. Added some instructions.

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,6 +6,16 @@ class Home extends Component{
         return (
             <Box sx={{ height: 400, width: '100%' }}>
                 <h1>Welcome to the Meter Manager Application</h1>
+                <p>
+                    This application facilites reporting of water management operations
+                    to State agencies (need details).
+                </p>
+                <h3>Instructions</h3>
+                <p>
+                    Test to see how this works
+                </p>
+                <h3>Settings</h3>
+                <p>Settings are controlled from ...</p>
 
             </Box>
         );
