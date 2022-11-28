ad3deafdadcd57a0ded320f759b49b5582243d2b
Chris Cox <chrisc@dyacon.com>
2022-11-28 10:11:17 -0700

    Working on Login functionality

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,24 +1,61 @@
 //Login screen
 
-import {Box} from "@mui/material";
-//import './css/login.css'
+import { useState } from "react";
+import { Navigate } from "react-router-dom"
+import {Box, TextField, Button} from "@mui/material";
 
 export default function Login() {
+    const [formvals, setFormVals] = useState({user:'',password:''})
+    const [errormsg, setErrorMsg] = useState()
+
+    function handleChange(e) {
+        let newvals = formvals
+        newvals[e.target.name] = e.target.value
+        setFormVals(newvals)
+    }
+    
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log('Submitted')
+        handleLogin({status:500})
+    }
+
+    function handleLogin(r) {
+        //Successful authorization
+        if(r.status == 500){
+            //UseNavigate??
+            return (<Box></Box>)
+        }
+    }
+    
     return (
-        <Box sx={{ height: 400, width: '100%' }}>
+        <Box sx={{ height: 800, width: '100%' }}>
             <h1>Welcome to the PVACD Meter Manager</h1>
-            <p id="error_txt"></p> {/*Return error if problem with login*/}
-            <form>
-                <label>
-                    Username:
-                    <input name="username" type="email" />
-                </label>
-                <label>
-                    Password:
-                    <input name="pwd" type="email" />
-                </label>
-                <input type="submit" value="Login" />
-            </form>
+            <p id="error_txt">Nothing</p> {/*Return error if problem with login*/}
+            <Box component="form" autoComplete="off" onSubmit={handleSubmit} id="myform">
+                <div>
+                    <TextField
+                        required
+                        id="login-user-input"
+                        label="Username"
+                        margin="normal"
+                        name="user"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                <TextField
+                    required
+                    id="login-pw-input"
+                    label="Password"
+                    type="password"
+                    margin="normal"
+                    name="password"
+                    onChange={handleChange}
+                />
+                </div>
+                <Button type="submit" variant="contained">Login</Button>
+            </Box>
             <a>Forgot Password?</a>
         </Box>
     );
