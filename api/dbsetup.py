c5785a67b8651f038c8fddb9cd3d41b55abd0440
Zachary (Zach) Carpenter <Zachary.Carpenter@respec.com>
2023-08-15 10:52:03 -0500

    Update dbseeder to reflect changes made in deployed DB

--- api/dbsetup.py
+++ api/dbsetup.py
@@ -214,6 +214,10 @@ if testing:
         #     qry = 'COPY "PartsUsed"(meter_activity_id, part_id, count) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
         #     cursor.copy_expert(qry, f)
 
+        with open("api/data/devdata_chloridemeasurements.csv", "r") as f:
+            qry = 'COPY "WellMeasurements"(timestamp,value,well_id,observed_property_id,submitting_user_id,unit_id) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
+            cursor.copy_expert(qry, f)
+
 #Create geometries from location lat longs
 cursor.execute('update "Locations" set geom = ST_MakePoint(longitude,latitude)')
 
