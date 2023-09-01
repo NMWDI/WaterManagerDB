bf7245c32d658341b785cc815f95bc97bb486448
Chris Cox <chrisc@dyacon.com>
2023-09-01 10:39:43 -0600

    Updated monitoring wells to match new wellids in database

--- frontend/src/views/MonitoringWells/MonitoringWellsView.tsx
+++ frontend/src/views/MonitoringWells/MonitoringWellsView.tsx
@@ -15,22 +15,30 @@ interface MonitoredWell {
 }
 
 const monitoredWells: MonitoredWell[] = [
-    {id: 1, name: 'Poe Corn', datastream_id: 14474},
-    {id: 2, name: 'Artesia', datastream_id: 14475},
-    {id: 3, name: 'TransWestern', datastream_id: 14469},
-    {id: 4, name: 'Cottonwood', datastream_id: 14470},
-    {id: 5, name: 'LFD', datastream_id: 14472},
-    {id: 6, name: 'Greenfield', datastream_id: 14477},
-    {id: 7, name: 'Berrendo-Smith', datastream_id: 14471},
-    {id: 8, name: 'Orchard Park', datastream_id: 14476},
-    {id: 9, name: 'Bartlett', datastream_id: 24951},
-    {id: 10, name: 'Zumwalt', datastream_id: 14468},
+    {id: 1515, name: 'Poe Corn', datastream_id: 14474},
+    {id: 1524, name: 'Artesia', datastream_id: 14475},
+    {id: 1516, name: 'TransWestern', datastream_id: 14469},
+    {id: 1522, name: 'Cottonwood', datastream_id: 14470},
+    {id: 1518, name: 'LFD', datastream_id: 14472},
+    {id: 1520, name: 'Greenfield', datastream_id: 14477},
+    {id: 1517, name: 'Berrendo-Smith', datastream_id: 14471},
+    {id: 1519, name: 'Orchard Park', datastream_id: 14476},
+    {id: 1521, name: 'Bartlett', datastream_id: 24951},
+    {id: 1523, name: 'Zumwalt', datastream_id: 14468},
 ]
+
+function getDatastreamID(input_wellID: number | undefined): number | undefined {
+    let welldetails = monitoredWells.find(x => x.id == input_wellID)
+    if (welldetails) 
+        return welldetails.datastream_id
+    else
+        return welldetails
+}
 
 export default function MonitoringWellsView(){
     const [wellID, setWellID] = useState<number>()
     const manualWaterLevelMeasurements = useGetWaterLevels({well_id: wellID})
-    const st2WaterLevelMeasurements = useGetST2WaterLevels(wellID ? monitoredWells[wellID - 1].datastream_id : undefined)
+    const st2WaterLevelMeasurements = useGetST2WaterLevels(getDatastreamID(wellID))
     const createWaterLevel = useCreateWaterLevel()
 
     const [isNewMeasurementModalOpen, setIsNewMeasurementModalOpen] = useState<boolean>(false)
