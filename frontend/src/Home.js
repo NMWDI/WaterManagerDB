bff6d4f3f91ebee9ccb2b3e2d25213fb5a09a86a
Chris Cox <chrisc@dyacon.com>
2024-02-27 16:19:10 -0700

    Update version for deployment

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -9,6 +9,7 @@ class Home extends Component{
     render() {
 
         const versionHistory = [
+            "V0.1.24 - Add non-functional merge button for initial testing",
             "V0.1.23 - Prevent duplicate activities from being added",
             "V0.1.22 - Change ownership so there is now water_users and meter_owner",
             "V0.1.21 - Implement Degrees Minutes Seconds (DMS) for lat/long",
