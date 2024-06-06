97b4e6e7706f742ed34d546beb6502ade7c3893f
Chris Cox <chrisc@dyacon.com>
2024-06-06 11:49:24 -0600

    Finalized initial work order table column visibility and layout

--- frontend/src/views/WorkOrders/WorkOrdersTable.tsx
+++ frontend/src/views/WorkOrders/WorkOrdersTable.tsx
@@ -17,40 +17,29 @@ export default function WorkOrdersTable() {
     const columns: GridColDef[] = [
         { field: 'work_order_id', headerName: 'ID', width: 100 },
         { field: 'date_created', headerName: 'Date', width: 150 },
-        { field: 'meter_serial', headerName: 'Meter', width: 150 },
+        { field: 'meter_serial', headerName: 'Meter', width: 100 },
         { field: 'title', headerName: 'Title', width: 200 },
         { field: 'description', headerName: 'Description', width: 300 },
         { field: 'creator', headerName: 'Created By', width: 150 },
-        { field: 'status', headerName: 'Status', width: 150 },
-        { field: 'notes', headerName: 'Notes', width: 200 },
+        { field: 'status', headerName: 'Status', width: 100 },
+        { field: 'notes', headerName: 'Notes', width: 300 },
         //{ field: 'activityIds', headerName: 'Activity IDs', width: 200 },
         { field: 'assigned_user_id', headerName: 'Technician Assigned', width: 200 },
         { field: 'actions', headerName: 'Actions', width: 150 },
     ];
 
-    // Define columnVisibilityModel to control state of columns
-    const [columnVisibilityModel, setColumnVisibilityModel] = useState<GridColumnVisibilityModel>({
-        id: false,
-        date: true,
-        meter: true,
-        title: true,
-        description: true,
-        createdBy: true,
-        status: true,
-        notes: true,
-        activityIds: false,
-        technicianAssigned: false,
-        actions: true,
-    });
-
     return (
         <div style={{ height: 500, width: '100%' }}>
             <DataGrid 
                 rows={workOrderList.data ?? []}
+                getRowHeight={() => 'auto'}
                 getRowId={(row) => row.work_order_id} 
-                columns={columns} 
-                columnVisibilityModel={columnVisibilityModel}
-                onColumnVisibilityModelChange={(newModel) => setColumnVisibilityModel(newModel)}
+                columns={columns}
+                initialState={
+                    {
+                        columns: {columnVisibilityModel: {work_order_id: false, creator: false, activityIds: false}}
+                    }
+                } 
             />
         </div>
     );
