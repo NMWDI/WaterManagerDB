494ffc96b55bcb10dfc9aa505533a2178b135c25
Chris Cox <chrisc@dyacon.com>
2024-06-11 11:53:11 -0600

    Added editable user list to work order UI

--- frontend/src/views/WorkOrders/WorkOrdersTable.tsx
+++ frontend/src/views/WorkOrders/WorkOrdersTable.tsx
@@ -6,18 +6,36 @@ I anticipate this component will be self-contained including the ability to add 
 import React from 'react';
 import { useState } from 'react';
 import { DataGrid, GridColDef, GridRowModel } from '@mui/x-data-grid';
-import { useGetWorkOrders, useUpdateWorkOrder } from '../../service/ApiServiceNew';
+import { useGetWorkOrders, useUpdateWorkOrder, useGetUserList } from '../../service/ApiServiceNew';
 import { WorkOrderStatus } from '../../enums';
 
 export default function WorkOrdersTable() {
     const [workOrderFilters, setWorkOrderFilters] = useState<WorkOrderStatus[]>([WorkOrderStatus.Open, WorkOrderStatus.Review, WorkOrderStatus.Closed]);
     const workOrderList = useGetWorkOrders(workOrderFilters);
-    const updateWorkOrder = useUpdateWorkOrder();  
+    const updateWorkOrder = useUpdateWorkOrder();
+    const userList = useGetUserList();  
+
+    function getUserFromID(id: number|undefined) {
+        return userList.data?.find(user => user.id === id)?.full_name ?? "";
+    }
+    
+    function getUserIDfromName(name: string) {
+        return userList.data?.find(user => user.full_name === name)?.id ?? 0;
+    }
 
     function handleRowUpdate(updatedRow: GridRowModel, originalRow: GridRowModel): Promise<GridRowModel> {
         //Determine what field has changed and update the work order
-        const updatedField = Object.keys(updatedRow).find(key => updatedRow[key] !== originalRow[key]); 
-        const work_order_update = {work_order_id: updatedRow.work_order_id, [updatedField as string]: updatedRow[updatedField as string]};
+        const updatedField = Object.keys(updatedRow).find(key => updatedRow[key] !== originalRow[key]);
+        let field_data = null;
+        
+        //If field is assigned_user_id, convert the name to an id
+        if (updatedField === 'assigned_user_id') {
+            field_data = getUserIDfromName(updatedRow.assigned_user_id as string);
+        } else {
+            field_data = updatedRow[updatedField as string];
+        }
+        
+        const work_order_update = {work_order_id: updatedRow.work_order_id, [updatedField as string]: field_data};
         console.log("Updating work order", work_order_update);
 
         //Create a promise to update the work order
@@ -40,7 +58,15 @@ export default function WorkOrdersTable() {
         { field: 'status', headerName: 'Status', width: 125, type: 'singleSelect', valueOptions: ['Open', 'Review', 'Closed'], editable: true},
         { field: 'notes', headerName: 'Notes', width: 300, editable: true},
         //{ field: 'activityIds', headerName: 'Activity IDs', width: 200 },
-        { field: 'assigned_user_id', headerName: 'Technician Assigned', width: 200 },
+        { 
+            field: 'assigned_user_id', 
+            headerName: 'Technician Assigned', 
+            width: 200, 
+            valueGetter: (id) => getUserFromID(id as number),
+            type: 'singleSelect',
+            valueOptions: userList.data?.map(user => user.full_name) ?? [], 
+            editable: true
+        },
         { field: 'actions', headerName: 'Actions', width: 150 },
     ];
 
