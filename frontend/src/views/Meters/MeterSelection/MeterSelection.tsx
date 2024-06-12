04a25e41f1cd6e49669f5dfb648917a9badd0fd3
Chris Cox <chrisc@dyacon.com>
2024-06-12 11:19:40 -0600

    Added toggle buttons to meter table for status filter, not yet functional

--- frontend/src/views/Meters/MeterSelection/MeterSelection.tsx
+++ frontend/src/views/Meters/MeterSelection/MeterSelection.tsx
@@ -5,7 +5,7 @@ import MeterSelectionTable from './MeterSelectionTable'
 import MeterSelectionMap from './MeterSelectionMap'
 import TabPanel from '../../../components/TabPanel'
 
-import { Box, Tabs, Tab, TextField, Grid, Card, CardContent, CardHeader } from '@mui/material'
+import { Box, Tabs, Tab, TextField, Grid, Card, CardContent, CardHeader, Button, ToggleButtonGroup, ToggleButton } from '@mui/material'
 import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
 
 interface MeterSelectionProps {
@@ -20,35 +20,54 @@ export default function MeterSelection({onMeterSelection, setMeterAddMode}: Mete
     const [meterSearchQuery, setMeterSearchQuery] = useState<string>('')
 
     return (
-            <Card sx={{height: '100%'}}>
-                <CardHeader
-                    title={
-                        <div className="custom-card-header">
-                            <span>All Meters</span>
-                            <FormatListBulletedOutlinedIcon/>
-                        </div>
-                    }
-                    sx={{mb: 0, pb: 0}}
-                />
-                <CardContent sx={{height: '100%'}}>
-                    <Grid container>
-                        <Grid item xs={9}>
+        <Card sx={{height: '100%'}}>
+            <CardHeader
+                title={
+                    <div className="custom-card-header">
+                        <span>All Meters</span>
+                        <FormatListBulletedOutlinedIcon/>
+                    </div>
+                }
+                sx={{mb: 0, pb: 0}}
+            />
+            <CardContent sx={{height: '100%'}}>
+                <Grid container justifyContent="space-between">
+                    <Grid item xs={4}>
                         <Tabs value={currentTabIndex} onChange={handleTabChange} >
                             <Tab label="Meter List" />
                             <Tab label="Meter Map" />
                         </Tabs>
-                        </Grid>
-                        <Grid item xs={3}>
-                            <TextField
-                                sx={{mt: 1}}
-                                label="Search Meter"
-                                variant="outlined"
-                                size="small"
-                                value={meterSearchQuery}
-                                onChange={(e) => {setMeterSearchQuery(e.target.value)}}
-                            />
-                        </Grid>
                     </Grid>
+                    <Grid item xs={3}>
+                        <TextField
+                            sx={{mt: 1}}
+                            label="Search Meter"
+                            variant="outlined"
+                            size="small"
+                            value={meterSearchQuery}
+                            onChange={(e) => {setMeterSearchQuery(e.target.value)}}
+                        />
+                    </Grid>
+                    <Grid item sx={{mt: 1}}>
+                        <ToggleButtonGroup
+                            value={[true, false, false]}
+                            exclusive={false}
+                            onChange={() => {console.log('clicked')}}
+                            size='small'
+                            aria-label="button group"
+                        >
+                            <ToggleButton value="installed" aria-label="installed">
+                                Installed
+                            </ToggleButton>
+                            <ToggleButton value="stored" aria-label="stored">
+                                Stored
+                            </ToggleButton>
+                            <ToggleButton value="other" aria-label="other">
+                                Other
+                            </ToggleButton>
+                        </ToggleButtonGroup>
+                    </Grid>
+                </Grid>
 
                 <Box sx={{height: '89%'}}>
                     <TabPanel currentTabIndex={currentTabIndex} tabIndex={0}>
