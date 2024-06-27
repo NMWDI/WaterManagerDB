2a04464a99bcaae72f72b9a0750d48b61977334a
Chris Cox <chrisc@dyacon.com>
2024-06-27 09:35:32 -0600

    Change order of monitoring wells

--- frontend/src/views/MonitoringWells/MonitoringWellsView.tsx
+++ frontend/src/views/MonitoringWells/MonitoringWellsView.tsx
@@ -18,15 +18,15 @@ interface MonitoredWell {
 
 const monitoredWells: MonitoredWell[] = [
     {id: 1515, name: 'Poe Corn', datastream_id: 14474},
-    {id: 1524, name: 'Artesia', datastream_id: 14475},
     {id: 1516, name: 'TransWestern', datastream_id: 14469},
-    {id: 1522, name: 'Cottonwood', datastream_id: 14470},
-    {id: 1518, name: 'LFD', datastream_id: 14472},
-    {id: 1520, name: 'Greenfield', datastream_id: 14477},
     {id: 1517, name: 'Berrendo-Smith', datastream_id: 14471},
+    {id: 1518, name: 'LFD', datastream_id: 14472},
     {id: 1519, name: 'Orchard Park', datastream_id: 14476},
+    {id: 1520, name: 'Greenfield', datastream_id: 14477},
     {id: 1521, name: 'Bartlett', datastream_id: 24951},
+    {id: 1522, name: 'Cottonwood', datastream_id: 14470},
     {id: 1523, name: 'Zumwalt', datastream_id: 14468},
+    {id: 1524, name: 'Artesia A', datastream_id: 14475}
 ]
 
 function getDatastreamID(input_wellID: number | undefined): number | undefined {
