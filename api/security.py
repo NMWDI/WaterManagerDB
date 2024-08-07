786dd9b6d356ceadb30aecdedef4b17b84b4aad3
Chris Cox <chrisc@dyacon.com>
2024-08-07 14:21:57 -0600

    Changed token expiration to 8 hr

--- api/security.py
+++ api/security.py
@@ -18,7 +18,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = "09d25e194fbb6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_HOURS = 3
+ACCESS_TOKEN_EXPIRE_HOURS = 8
 
 invalid_credentials_exception = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
