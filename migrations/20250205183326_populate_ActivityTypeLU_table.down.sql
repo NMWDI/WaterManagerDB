83d8889354041dbe3ed6ea6bcfdd5cdd28614dad
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-02-05 13:47:31 -0600

    [/migrations] Add pop script for LandOwners table

--- migrations/20250205183326_populate_ActivityTypeLU_table.down.sql
+++ migrations/20250205183326_populate_ActivityTypeLU_table.down.sql
@@ -1,3 +1,18 @@
 DELETE FROM public."ActivityTypeLU"
 WHERE
     id IN (1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+
+SELECT
+    setval (
+        'public."ActivityTypeLU_id_seq"',
+        COALESCE(
+            (
+                SELECT
+                    MAX(id)
+                FROM
+                    public."ActivityTypeLU"
+            ),
+            1
+        ),
+        false
+    );
