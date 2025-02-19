f8a94d0d327f3b97a336946bea4a220198e13860
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-02-18 20:42:42 -0600

    [MeterListQuery] Switch to new enum name

--- frontend/src/interfaces/MeterListQuery.ts
+++ frontend/src/interfaces/MeterListQuery.ts
@@ -1,8 +1,8 @@
-import { SortDirection, MeterListSortBy } from "../enums";
+import { SortDirection, MeterSortByField } from "../enums";
 
 export interface MeterListQuery {
   search_string: string;
-  sort_by: MeterListSortBy;
+  sort_by: MeterSortByField;
   sort_direction: SortDirection;
   limit: number;
   offset: number;
