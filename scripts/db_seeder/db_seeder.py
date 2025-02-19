107f01c8d46bef77893394dc64dc295761da3eb0
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-02-19 09:46:03 -0600

    [/db_seeder] Add pop well_measurements script

--- scripts/db_seeder/db_seeder.py
+++ scripts/db_seeder/db_seeder.py
@@ -3,6 +3,7 @@ from populate_users import populate_users
 from populate_land_owners import populate_land_owners
 from populate_locations import populate_locations
 from populate_wells import populate_wells
+from populate_well_measurements import populate_well_measurements
 
 if __name__ == "__main__":
     init_db()
@@ -10,3 +11,4 @@ if __name__ == "__main__":
     populate_land_owners(25)
     populate_locations()
     populate_wells()
+    populate_well_measurements()
