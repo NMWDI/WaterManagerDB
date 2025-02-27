660a1b172804e6f98ad472c4c2a772b5f251ca59
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-02-27 12:39:44 -0600

    [DataStreamUtils] Update func to pull from hardcodeded list

--- frontend/src/utils/DataStreamUtils.ts
+++ frontend/src/utils/DataStreamUtils.ts
@@ -1,11 +1,35 @@
-import { MonitoredWell } from "../interfaces";
+// import { MonitoredWell } from "../interfaces";
 
-export const getDataStreamId = (
-  wells: MonitoredWell[],
-  wellId: number | undefined,
-): number | undefined => {
-  const wellDetails = wells.find((x) => x.id === wellId);
-  if (!wellDetails) return undefined;
-  if (wellDetails.datastream_id === -999) return undefined;
-  return wellDetails.datastream_id;
+const monitoredWellDataStreamIds: Record<number, number> = {
+  1515: 25089,
+  1516: 25083,
+  1517: 25086,
+  1518: 25087,
+  1519: 25091,
+  1520: 25092,
+  1521: 25084,
+  1522: 25085,
+  1523: 25082,
+  1524: 25090,
+  2596: -999,
+  2597: -999,
+  2598: -999,
+  2599: -999,
+  2600: -999,
+  2601: -999,
+  2602: -999,
 };
+
+export const getDataStreamId = (wellId: number): number | undefined => {
+  return monitoredWellDataStreamIds[wellId];
+};
+
+// export const getDataStreamId = (
+//   wells: MonitoredWell[],
+//   wellId: number | undefined,
+// ): number | undefined => {
+//   const wellDetails = wells.find((x) => x.id === wellId);
+//   if (!wellDetails) return undefined;
+//   if (wellDetails.datastream_id === -999) return undefined;
+//   return wellDetails.datastream_id;
+// };
