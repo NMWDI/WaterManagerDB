197e1694174726c5222ed3e60577e88dc9395900
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-03-11 15:16:43 -0500

    [/components] Rm unused imports

--- frontend/src/components/DMSentry.tsx
+++ frontend/src/components/DMSentry.tsx
@@ -1,82 +1,47 @@
 /*
-
-This component is a UI that allows a user to enter a latitude or longitude using 
-degrees, minutes, and seconds.
-
+  This component is a UI that allows a user to enter a latitude or longitude using 
+  degrees, minutes, and seconds.
 */
 
-import React, { useEffect, useState } from 'react';
-//import { TextField } from '@mui/material'
+import { useEffect, useState } from "react";
 
-//Create a component that takes a label (latitude or longitude) and a callback function
-//that will be called when the user enters a valid latitude or longitude.
+// Create a component that takes a label (latitude or longitude) and a callback function
+// That will be called when the user enters a valid latitude or longitude.
 export default function DMSentry({ dms_label, onChange }: any) {
-    const [degrees, setDegrees] = useState<number>(0);
-    const [minutes, setMinutes] = useState<number>(0);
-    const [seconds, setSeconds] = useState<number>(0);
-    const [decimal_degrees, setDecimalDegrees] = useState<Number>(0);
+  const [degrees, setDegrees] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+  const [decimal_degrees, setDecimalDegrees] = useState<Number>(0);
 
-    function calculateDecimalDegrees(d: number ,m: number, s: number) {
-        return d + m/60 + s/3600;
-    }
-    
-    useEffect(() => {
-        setDecimalDegrees(calculateDecimalDegrees(degrees, minutes, seconds));
-        onChange(decimal_degrees);
-    }, [degrees, minutes, seconds])
+  const calculateDecimalDegrees = (d: number, m: number, s: number) =>
+    d + m / 60 + s / 3600;
 
-    // //This function is called when the user enters a valid latitude or longitude.
-    // //It calls the callback function that was passed in as a prop.
-    // const handleEntry = () => {
-    //     //If the user entered a valid latitude or longitude, call the callback function.
-    //     if (validateEntry()) {
-    //         props.onEntry(degrees, minutes, seconds);
-    //     }
-    // }
+  useEffect(() => {
+    setDecimalDegrees(calculateDecimalDegrees(degrees, minutes, seconds));
+    onChange(decimal_degrees);
+  }, [degrees, minutes, seconds]);
 
-    // //This function validates the latitude or longitude entered by the user.
-    // //It returns true if the user entered a valid latitude or longitude.
-    // const validateEntry = () => {
-    //     //If the user didn't enter a value for degrees, minutes, or seconds, return false.
-    //     if (degrees === '' || minutes === '' || seconds === '') {
-    //         return false;
-    //     }
-
-    //     //If the user entered a value for degrees, minutes, or seconds that isn't a number, return false.
-    //     if (isNaN(Number(degrees)) || isNaN(Number(minutes)) || isNaN(Number(seconds))) {
-    //         return false;
-    //     }
-
-    //     //If the user entered a value for degrees, minutes, or seconds that is less than 0, return false.
-    //     if (Number(degrees) < 0 || Number(minutes) < 0 || Number(seconds) < 0) {
-    //         return false;
-    //     }
-
-    //     //If the user entered a value for degrees that is greater than 180, return false.
-    //     if (Number(degrees) > 180) {
-    //         return false;
-    //     }
-
-    //     //If the user entered a value for minutes or seconds that is greater than 59, return false.
-    //     if (Number(minutes) > 59 || Number(seconds) > 59) {
-    //         return false;
-    //     }
-
-    //     //If the user entered a value for degrees that is greater than 90, return false.
-    //     if (props.label === 'Latitude' && Number(degrees) > 90) {
-    //         return false;
-    //     }
-
-    //     //If the user entered a value for degrees that is greater than 180, return false.
-    //     if (props.label === 'Longitude' && Number(degrees) > 180) {
-    //         return false;
-    //     }
-
-    return (
-        <div>
-            {dms_label}:
-            <input value={ degrees } onChange={ (e: any) => setDegrees(Number(e.target.value)) } type="number"/>&deg;
-            <input value={ minutes } onChange={ (e: any) => setMinutes(Number(e.target.value)) } type="number"/>&prime;
-            <input value={ seconds } onChange={ (e: any) => setSeconds(Number(e.target.value)) } type="number"/>&Prime;&Prime;
-        </div>
-    )}
+  return (
+    <div>
+      {dms_label}:
+      <input
+        value={degrees}
+        onChange={(e: any) => setDegrees(Number(e.target.value))}
+        type="number"
+      />
+      &deg;
+      <input
+        value={minutes}
+        onChange={(e: any) => setMinutes(Number(e.target.value))}
+        type="number"
+      />
+      &prime;
+      <input
+        value={seconds}
+        onChange={(e: any) => setSeconds(Number(e.target.value))}
+        type="number"
+      />
+      &Prime;&Prime;
+    </div>
+  );
+}
--- frontend/src/components/MergeWellModal.tsx
+++ frontend/src/components/MergeWellModal.tsx
@@ -1,103 +1,114 @@
-//Modal for merging one well into another. Used on Wells Detail Component
-
-import {
-    Box,
-    Modal,
-    Button,
-    Grid
-} from "@mui/material";
-import { useState, useEffect } from 'react'
-import React from 'react'
+import { Box, Modal, Button, Grid } from "@mui/material";
+import { useState, useEffect } from "react";
 import { useMergeWells } from "../service/ApiServiceNew";
 import WellSelection from "./WellSelection";
-import { Well } from '../interfaces'
-import { set } from "react-hook-form";
+import { Well } from "../interfaces";
 
-//Interface for Modal
-//Errors are handled by the API service
-//handleSuccess is passed from parent and is likely used for a snackbar message
-interface NewMeasurementModalProps {
-    isWellMergeModalOpen: boolean
-    handleCloseMergeModal: () => void
-    handleSuccess: () => void
-    mergeWell_raNumber: string
-  }
+export function MergeWellModal({
+  isWellMergeModalOpen,
+  handleCloseMergeModal,
+  handleSuccess,
+  mergeWell_raNumber,
+}: {
+  isWellMergeModalOpen: boolean;
+  handleCloseMergeModal: () => void;
+  handleSuccess: () => void;
+  mergeWell_raNumber: string;
+}) {
+  const [targetWell, setTargetWell] = useState<Well | null>(null);
+  const [disableMergeButton, setDisableMergeButton] = useState<boolean>(true);
 
-export function MergeWellModal({isWellMergeModalOpen, handleCloseMergeModal, handleSuccess, mergeWell_raNumber}: NewMeasurementModalProps) {
-    //Create state variable targetWell
-    const [targetWell, setTargetWell] = useState<Well | null>(null)
-    const [disableMergeButton, setDisableMergeButton] = useState<boolean>(true)
+  const mergeWells = useMergeWells(handleSuccess);
 
-    const mergeWells = useMergeWells(handleSuccess)
+  const handleSubmit = () => {
+    let targetWell_raNumber = targetWell?.ra_number ?? "";
+    console.log(
+      "Merging well: " + mergeWell_raNumber + " into " + targetWell_raNumber,
+    );
 
-    //Create function to handle submit
-    const handleSubmit = () => {
-        let targetWell_raNumber = targetWell?.ra_number ?? ''
-        console.log('Merging well: ' + mergeWell_raNumber + ' into ' + targetWell_raNumber)
-
-        //Prevent merge if mergeWell and targetWell are the same
-        if (mergeWell_raNumber == targetWell_raNumber) {
-            console.log('Cannot merge well into itself')
-            setTargetWell(null)
-            handleCloseMergeModal()
-            return
-        }
-
-        mergeWells.mutate({merge_well: mergeWell_raNumber, target_well: targetWell_raNumber})
-        setTargetWell(null)
-        handleCloseMergeModal()
+    //Prevent merge if mergeWell and targetWell are the same
+    if (mergeWell_raNumber == targetWell_raNumber) {
+      console.log("Cannot merge well into itself");
+      setTargetWell(null);
+      handleCloseMergeModal();
+      return;
     }
 
-    //Clear targetWell on cancel button
-    const handleCancelMergeModal = () => {
-        setTargetWell(null)
-        handleCloseMergeModal()
+    mergeWells.mutate({
+      merge_well: mergeWell_raNumber,
+      target_well: targetWell_raNumber,
+    });
+    setTargetWell(null);
+    handleCloseMergeModal();
+  };
+
+  //Clear targetWell on cancel button
+  const handleCancelMergeModal = () => {
+    setTargetWell(null);
+    handleCloseMergeModal();
+  };
+
+  //Enable merge button if targetWell is selected
+  useEffect(() => {
+    if (targetWell) {
+      setDisableMergeButton(false);
+    } else {
+      setDisableMergeButton(true);
     }
+  }, [targetWell]);
 
-    //Enable merge button if targetWell is selected
-    useEffect(() => {
-        if (targetWell) {
-            setDisableMergeButton(false)
-        }else{
-            setDisableMergeButton(true)
-        }
-    }, [targetWell])
-
-    return (
-        <Modal
-            open={isWellMergeModalOpen}
-            onClose={handleCloseMergeModal}
-        >
-            <Box style={{
-                position: 'absolute' ,
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                backgroundColor: 'white',
-                paddingRight: 20,
-                paddingBottom: 20,
-                boxShadow: '24',
-                borderRadius: 15,
-                paddingLeft: 25}}
-            >
-                <Grid item xs={12}>
-                    <h2>Well Merge</h2>
-                    <Grid container item xs={12} sx={{mr: 'auto', ml: 'auto', mb: 2}}>
-                        Merge all meter history from {mergeWell_raNumber} into target well:
-                    </Grid>
-                    <Grid container item xs={12} sx={{mr: 'auto', ml: 'auto', mb: 2}}>
-                        <WellSelection selectedWell={targetWell} onSelection={setTargetWell} />
-                    </Grid>
-                    <Grid container item spacing={2} xs={6} sx={{mr: 'auto', ml: 'auto'}}>
-                        <Grid item>
-                            <Button type="submit" variant="contained" onClick={handleSubmit} disabled={disableMergeButton}>Merge</Button>
-                        </Grid>    
-                        <Grid item>
-                            <Button variant="contained" onClick={handleCancelMergeModal}>Cancel</Button>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Modal>
-    )
+  return (
+    <Modal open={isWellMergeModalOpen} onClose={handleCloseMergeModal}>
+      <Box
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          backgroundColor: "white",
+          paddingRight: 20,
+          paddingBottom: 20,
+          boxShadow: "24",
+          borderRadius: 15,
+          paddingLeft: 25,
+        }}
+      >
+        <Grid item xs={12}>
+          <h2>Well Merge</h2>
+          <Grid container item xs={12} sx={{ mr: "auto", ml: "auto", mb: 2 }}>
+            Merge all meter history from {mergeWell_raNumber} into target well:
+          </Grid>
+          <Grid container item xs={12} sx={{ mr: "auto", ml: "auto", mb: 2 }}>
+            <WellSelection
+              selectedWell={targetWell}
+              onSelection={setTargetWell}
+            />
+          </Grid>
+          <Grid
+            container
+            item
+            spacing={2}
+            xs={6}
+            sx={{ mr: "auto", ml: "auto" }}
+          >
+            <Grid item>
+              <Button
+                type="submit"
+                variant="contained"
+                onClick={handleSubmit}
+                disabled={disableMergeButton}
+              >
+                Merge
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" onClick={handleCancelMergeModal}>
+                Cancel
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+    </Modal>
+  );
 }
